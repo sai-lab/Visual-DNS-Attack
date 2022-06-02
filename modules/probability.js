90b0063cc932ab9c2cb45a3293d640e03b51b8d9
pikapika2 <yossii0108@gmail.com>
2022-06-02 18:41:22 +0900

    add probability file
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

---
+++ modules/probability.js
@@ -0,0 +1,20 @@
+
+
+export function probability(a, b){
+
+	let divisor = gcb(a, b);
+
+	return "攻撃成功確率" + a / divisor + "/" + b / divisor;
+}
+
+function gcb(a, b){
+	let num1 = a;
+	let num2 = b;
+
+	while(num2 !== 0){
+		let m = num1 % num2;
+		num1 = num2;
+		num2 = m;
+	}
+	return num1;
+}
